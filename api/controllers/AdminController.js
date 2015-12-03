8326c496222966175e1baeff08dee752f0275a49
Frédéric Woelffel <woelffel.f@gmail.com>
2015-12-03 22:58:33 +0100

    Implemented AdminController.createSession

--- api/controllers/AdminController.js
+++ api/controllers/AdminController.js
@@ -1,3 +1,7 @@
+'use strict';
+
+var sha1 = require('sha1')
+
 /**
  * AdminController
  *
@@ -22,6 +26,32 @@ module.exports = {
    * @param res
    */
   ,createSession: function(req, res) {
+
+    var adminName = req.param('adminName');
+    var adminPassword = req.param('adminPassword');
+
+    Admin.findByName(adminName, function(err, admin) {
+      if(err) {
+        res.error(500);
+      }
+      else if ('undefined' === typeof admin) {
+        res.send(404);
+      }
+      else {
+        // There is an admin with this name
+        // Checking password
+        if(sha1(adminPassword) === admin.password) {
+          // Passwords match
+          req.session.user = admin.toJSON();
+          res.send(200);
+        }
+        else {
+          // Password doesn't match
+          res.send(404);
+        }
+      }
+    });
+
     res.send(501);
   }
 };
